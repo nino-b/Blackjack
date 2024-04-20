11d1ceec1d8ca7f82de7b5c6e617bac877facf7c
Nino Burtchuladze <burtchuladze.dev@gmail.com>
2024-04-20 21:05:50 +0400

    DialogManager finished. Tests needed

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+/** */
 //import Router from "./src/Router.js";
 //
 //window.addEventListener('DOMContentLoaded', async () => {
 //    /* loadData(); */
 //    app.router.init();/* <-- Not Correct !!! */
 //})
+'use strict';
 
 import DialogManager from "./src/DialogManager.js";
+const openDialogBtns = document.querySelectorAll('.dialog-btn');
+const dialogBoxes = document.querySelectorAll('.dialog-box');
+const closeDialogBtns = document.querySelectorAll('.close-dialog-btn');
 
-new DialogManager();
+const datasetCloseAction = 'close-dialog';
+const closingAnimation = 'dialog-closing';
+
+new DialogManager({openDialogBtns, dialogBoxes, closeDialogBtns}, {datasetCloseAction, closingAnimation});
